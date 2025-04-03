36e26fbaf2af505ab7c84f515a057ed8a980515f
OscuriorGG <115103001+OscuriorGG@users.noreply.github.com>
2025-04-03 09:16:11 +0200

    Update TreeTab.lua

--- src/Classes/TreeTab.lua
+++ src/Classes/TreeTab.lua
@@ -285,8 +285,15 @@ local TreeTabClass = newClass("TreeTab", "ControlHost", function(self, build)
 end)
 
 function TreeTabClass:Draw(viewPort, inputEvents)
-	self.anchorControls.x = viewPort.x + 4
-	self.anchorControls.y = viewPort.y + viewPort.height - 24
+	-- Primero dibujamos la imagen de fondo
+    SetDrawLayer(0)
+    SetDrawColor(1, 1, 1, 0.3) -- El último valor (0.3) controla la transparencia
+    -- Asumiendo que la imagen está en la carpeta TreeData
+    DrawImage("TreeData/background.png", viewPort.x, viewPort.y, viewPort.width, viewPort.height)
+
+    -- Continúa con el código original
+    self.anchorControls.x = viewPort.x + 4
+    self.anchorControls.y = viewPort.y + viewPort.height - 24
 
 	for id, event in ipairs(inputEvents) do
 		if event.type == "KeyDown" then
